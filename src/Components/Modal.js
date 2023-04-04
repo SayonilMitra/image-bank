7ec8088a204a4444f44b633db20861da1f6f9590
Sayonil Mitra <sayonilmitra@gmail.com>
2023-04-04 16:59:08 +0530

    modal can now get user input

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,14 +1,18 @@
 import './Modal.css'
+import { useState } from 'react'
 
 export default function Modal({ setModal }) {
+    let [imageLabel, setLabel] = useState('')
+    let [imageUrl, setUrl] = useState('')
+
     return <div className="modal">
         <label>
             Label <br />
-            <input />
+            <input onChange={(e) => { setLabel(e.target.value) }} />
         </label> <br />
         <label>
             Photo URL <br />
-            <input />
+            <input onChange={(e) => { setUrl(e.target.value) }} />
         </label><br />
         <div className='modal-btn'>
             <button onClick={(e) => { setModal(false) }} id='modal-cancel'>Cancel</button>
